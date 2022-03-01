b90caf1413d2bfde606af0adad67b11b940698ff
ankydv <88019262+ankydv@users.noreply.github.com>
2022-03-01 23:04:00 +0530

    Update merge_sort.c

--- c/merge_sort.c
+++ c/merge_sort.c
@@ -1,76 +1,87 @@
 #include<stdio.h>
+#include<conio.h>
 
-void display(int*,int,int );
+void display(int*,int);
 void merge_sort(int*,int,int);
-void merge(int*,int, int, int);
+void merge(int*,int,int,int);
 
 int main(){
-printf("Welcome to Merge Sort Algorithm");
-int n,i,arr[100];
-printf("\nEnter the size of array : ");
-scanf("%d",&n);
-printf("\nEnter all the elements of the array\n");
-for(i=0;i<n;i++){
-    scanf("%d",&arr[i]);
-}
-int* p=arr;
-printf("\nInput array : ");
-display(p,0,n-1);
-merge_sort(p,0,n-1);
-printf("\nSorted Array : ");
-display(p,0,n-1);
-
-return 0;
+    int n;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("\nEnter elements of the array : ");
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("\nInput Array : ");
+    display(arr,n);
+    int *p=arr;
+    merge_sort(p,0,n-1);
+    printf("\nSorted Array : ");
+    display(p,n);
 }
 
-void display(int* arr,int s,int e){
-    for(int i=s;i<=e;i++){
-        printf("%d ",arr[i]);
+void display(int *p,int n){
+    int i=0;
+    while(i<n){
+        printf("%d ",p[i]);
+        i++;
     }
 }
 
-void merge_sort(int *arr,int s,int e){
+void merge_sort(int *p,int s,int e){
     if(s<e){
-        int m=(s+e)/2;
-        merge_sort(arr,s,m);
-        merge_sort(arr,m+1,e);
-        merge(arr,s,m,e);
+        int m = (s+e)/2;
+        merge_sort(p,s,m);
+        merge_sort(p,m+1,e);
+        merge(p,s,m,e);
+        
+        
+        
+        
+        
+        
     }
 }
 
-void merge(int *a,int s,int m,int e){
-    int n1=m-s+1,
-    n2=e-m,
-    a1[n1],
-    a2[n2],
-    i=0,j=0,k=s;
-    for(i=0;i<n1;i++){
-        a1[i]=a[s+i];
-        if(i<=n2)
-            a2[i]=a[m+1+i];
+void merge(int *p,int s, int m , int e){
+    int n1 = m-s+1,
+    n2 = e-m;
+    int a[n1] , b[n2];
+    int i = 0, j = 0;
+    
+    while(i<n1){
+        a[i] = p[s+i];
+        i++;
     }
-    while(i<=n1 && j<=n2){
-        if(a1[i]<a2[j]){
-            a[k]=a1[i];
+    while(j<n2){
+        b[j] = p[m+1+j];
+        j++;
+    }
+
+    i = 0,j=0;
+    int k=s;
+    
+    while(i<n1 && j<n2){
+        if(a[i] <= b[j]){
+            p[k] = a[i];
             i++;
         }
         else{
-            a[k]=a2[j];
+            p[k] = b[j];
             j++;
         }
         k++;
     }
-    if(i<=n1){
-        while(i<=n1){
-            a[k]=a1[i];
-            i++,k++;
-        }
+    
+    while(i<n1){
+        p[k] = a[i];
+        i++;
     }
-    else{
-        while(j<=n2){
-            a[k]=a2[j];
-            j++,k++;
-        }
+    while(j<n2){
+        p[k] = b[j];
+        j++;
     }
+    
 }
-
