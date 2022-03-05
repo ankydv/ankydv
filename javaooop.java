3bf19d8bec3cb3016dbd5e1fa35e5044306f287e
ankydv <88019262+ankydv@users.noreply.github.com>
2022-03-05 15:58:06 +0530

    Create javaooop.java

---
+++ javaooop.java
@@ -0,0 +1,18 @@
+class Student{
+    String name;
+    int age;
+    public Student(String name,int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+}
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		Student obj = new Student('Bapan',20);
+		System.out.print(obj.name;
+	}
+}
