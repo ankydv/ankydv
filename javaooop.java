340bfa1c377060f48686a82173d8ef53b1028d70
ankydv <88019262+ankydv@users.noreply.github.com>
2022-03-06 09:35:54 +0530

    Update javaooop.java

--- javaooop.java
+++ javaooop.java
@@ -5,14 +5,16 @@ class Student{
         this.name = name;
         this.age = age;
     }
-    
+    public void show(){
+        System.out.print("Name : "+this.name+"\nAge : "+this.age);
+    }
 }
 
 
 public class Main
 {
 	public static void main(String[] args) {
-		Student obj = new Student('Bapan',20);
-		System.out.print(obj.name;
+		Student obj = new Student("Bapan",20);
+		obj.show();
 	}
 }
