88f49f4ce83887a84c99ddf42f5ff33042e47319
ankydv <88019262+ankydv@users.noreply.github.com>
2022-08-21 15:29:59 +0530

    Add files via upload

---
+++ java/assignment/Five.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Five {
+    public static void main(String[] args) {      
+          
+        //Initialize array   
+        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};   
+          
+        System.out.println("Elements in given array: ");  
+        //traverses whole array
+        for(int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i]+" ");
+        }  
+    }  
+}
---
+++ java/assignment/One.java
@@ -0,0 +1,16 @@
+package assignment;
+class TestArrayCopyDemo 
+{  
+    public static void main(String[] args) 
+{  
+        //declaring a source array  
+        char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',  
+                'i', 'n', 'a', 't', 'e', 'd' };  
+        //declaring a destination array  
+        char[] copyTo = new char[7];  
+        //copying array using System.arraycopy() method  
+        System.arraycopy(copyFrom,3,copyTo,0,7);  
+        //printing the destination array  
+        System.out.println(String.valueOf(copyTo));  
+    }  
+} 
